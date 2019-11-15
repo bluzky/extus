2fce6b466823ecb704f3f9763d4e18607336f29e
Aniruddha <aniruddha@oktalk.com>
2019-11-15 14:10:33 +0530

    Changing Access-Control-Allow-Origin to * instead of null (possible fix for dashboard ans upload issue?)

--- lib/utils.ex
+++ lib/utils.ex
@@ -25,7 +25,7 @@ defmodule ExTus.Utils do
 
   def put_cors_headers(conn) do
     conn
-    |> put_resp_header("Access-Control-Allow-Origin", "null")
+    |> put_resp_header("Access-Control-Allow-Origin", "*")
     |> put_resp_header("Access-Control-Expose-Headers", "Upload-Offset, Location, Upload-Length, Tus-Version, Tus-Resumable, Tus-Max-Size, Tus-Extension, Upload-Metadata")
   end
 end
