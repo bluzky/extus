faabe011b073a59cb957b33ce57840f35790cfc7
Rajat Chaudhary <rajat.chaudhary@kooapp.com>
2021-11-29 19:55:39 +0530

    Fix argument error while parsing upload metadata

--- lib/utils.ex
+++ lib/utils.ex
@@ -3,28 +3,31 @@ defmodule ExTus.Utils do
 
   def set_base_resp(conn) do
     conn
-    |> put_resp_header("Tus-Resumable", ExTus.Config.tus_api_version)
+    |> put_resp_header("Tus-Resumable", ExTus.Config.tus_api_version())
     |> put_resp_header("Cache-Control", "no-store")
   end
 
   def read_headers(conn) do
     conn.req_headers
     |> Enum.map(fn {k, v} -> {String.downcase(k), v} end)
-    |> Enum.into(Map.new)
+    |> Enum.into(Map.new())
   end
 
   def parse_meta_data(meta_str) do
     meta_str
     |> String.split(",")
-    |> Enum.map(fn item ->
-      String.split(item, " ", parts: 2)
-      |> List.to_tuple
+    |> Enum.flat_map(fn item ->
+      parts = String.split(item, " ", parts: 2)
+      if length(parts) == 2, do: [List.to_tuple(parts)], else: []
     end)
-    |> Enum.into(Map.new)
+    |> Enum.into(Map.new())
   end
 
   def put_cors_headers(conn) do
     conn
-    |> put_resp_header("Access-Control-Expose-Headers", "Upload-Offset, Location, Upload-Length, Tus-Version, Tus-Resumable, Tus-Max-Size, Tus-Extension, Upload-Metadata, URL")
+    |> put_resp_header(
+      "Access-Control-Expose-Headers",
+      "Upload-Offset, Location, Upload-Length, Tus-Version, Tus-Resumable, Tus-Max-Size, Tus-Extension, Upload-Metadata, URL"
+    )
   end
 end
